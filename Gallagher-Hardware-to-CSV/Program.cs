910c324b8b3ce4f2ac94f4303a4d9910940596ba
Mitchell-Lohmann <n11243252@qut.edu.au>
2024-08-11 11:55:16 +1000

    first commit

---
+++ Gallagher-Hardware-to-CSV/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gallagher_Hardware_to_CSV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
